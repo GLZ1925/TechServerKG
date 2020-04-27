34252257038b3dc9062d119d234e59112813313f
youlitaiGLZ <31872280+GLZ1925@users.noreply.github.com>
2020-04-27 18:02:54 +0800

    Add files via upload
    上传了程序的主要代码和代码运行所需要的txt文件
    neoScript.py 将txt文件中整理好的实体和关系转化成neo4j数据库支持的cypher脚本
    bot2neo.py 基于知识图谱的领域知识问答机器人实例

---
+++ bot2neo.py
@@ -0,0 +1,63 @@
+import thulac
+from py2neo import Graph,Node,Relationship,NodeMatcher
+
+
+# print(cut_text)
+entity_dict = {}
+entity_type_dict = {}
+def init():
+    with open("entity.txt","r",encoding="utf-8") as f:
+        for line in f.readlines():
+            line = line.strip('\n')
+            line = line.split('\t')
+            entity_dict[line[1]] = line[0]
+
+    with open("entity_type.txt", "r", encoding="utf-8") as f:
+        for line in f.readlines():
+            line = line.strip('\n')
+            line = line.split(' ')
+            entity_type_dict[line[0]] = line[1]
+
+def analyse_question(text,cutter):
+    cut_text = cutter.cut(text)
+    entity_list = []
+    entity_type_list = []
+    for seg in cut_text:
+        if seg[0] in entity_type_dict.keys():
+            entity_type_list.append(seg[0])
+        elif seg[0] in entity_dict.keys():
+            entity_list.append(seg[0])
+    # print(cut_text)
+    # print(entity_list)
+    # print(entity_type_list)
+    graph = Graph("http://localhost:7474", username="", password='')
+    for entity in entity_list:
+        for entity_type in entity_type_list:
+            # print("MATCH (entity1) - [rel] - (entity2:%s)  WHERE entity1.name ='%s' RETURN entity2.name"%(entity_type_dict[entity_type],entity))
+            ans = graph.run("MATCH (entity1) - [rel] - (entity2:%s)  WHERE entity1.name ='%s' RETURN entity2.name"%(entity_type_dict[entity_type],entity)).data()
+            if len(ans) is not 0:
+                response = "%s相关的%s有:"%(entity,entity_type)
+                # print(ans)
+                for dict in ans:
+                    response += dict['entity2.name'] + ","
+                response = response[:-1]
+                print(response)
+            else:
+                response = "%s相关的%s暂未有了解，我们会继续完善补充数据的！(≧∀≦)ゞ"%(entity,entity_type)
+                print(response)
+        if len(entity_type_list) == 0:
+            response = "您所提问%s的相关内容我们暂不了解，我们会继续完善补充数据的！(≧∀≦)ゞ"%(entity)
+            print(response)
+    if len(entity_list) == 0:
+        response = "您所提问的我们暂不了解，我们会继续完善补充数据的！(≧∀≦)ゞ"
+        print(response)
+
+if __name__ == "__main__":
+    init()
+    # text = "我想了解苏州瑞博公司的研究平台"
+    cutter = thulac.thulac(user_dict="user_dict.txt")
+    while True:
+        text = input('TechBot请您提问...>>')
+        #analyse_question(text,cutter=cutter)
+        cut_text = cutter.cut(text)
+        print(cut_text)
---
+++ neoScript.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import re
+from py2neo import Graph
+
+def scriptToNeo():
+    # 链接知识图谱，注意写上自己neo4j图数据库的用户名和密码
+    graph = Graph("http://localhost:7474", username="", password='')
+    # 根据cypher语法把我们要放入的实体和关系脚本创建好
+    create_entity_order = []
+    entity_label = {}
+    with open('小核酸实体.txt', 'r', encoding='UTF-8') as f:
+        for line in f.readlines():
+            count = 0
+            for value in line.split('\t'):
+                if count % 2 == 0:
+                    label = value
+                else:
+                    entity = value
+                    entity = entity.replace('\n', '')
+                count += 1
+            # print('label:',label)
+            # print('entity:',entity)
+            order = "create (n:%s {name:'%s'}) " % (label, entity)
+            entity_label[entity] = label
+            print(order)
+            create_entity_order.append(order)
+    relation_type = {
+        'concurrency': '概念上相关',
+        'close_to': '概念上相关',
+        'rely_on': '概念上相关',
+        'derive': '概念上相关',
+        'subclass': '概念上相关',
+        'cover': '概念上相关',
+        'synonymous': '概念上相关',
+        'produce': '物理上相关',
+        'develop': '物理上相关',
+        'belong': '物理上相关',
+        'own': '物理上相关',
+        'cooperate': '物理上相关',
+        'degradate': '物理上相关',
+        'build': '物理上相关',
+        'apply_to': '功能上相关',
+        'act_on': '功能上相关',
+        'induce': '功能上相关',
+        'restrain': '功能上相关',
+        'focus_on': '功能上相关'
+    }
+    with open('小核酸关系.txt', 'r', encoding='UTF-8') as f:
+        for line in f.readlines():
+            count = 0
+            for value in line.split('\t'):
+                if count % 3 == 0:
+                    entity1 = value
+                elif count % 3 == 1:
+                    relation = value
+                    relation = relation.replace('\n', '')
+                    relation_label = relation_type[relation]
+                elif count % 3 == 2:
+                    entity2 = value
+                    entity2 = entity2.replace('\n', '')
+                count += 1
+            order = "match (entity1:%s) ,(entity2:%s) where entity1.name = '%s' and entity2.name = '%s' create (entity1)-[r:%s {name:'%s'}]->(entity2)" \
+                    % (entity_label[entity1], entity_label[entity2], entity1, entity2, relation_label, relation)
+            print(order)
+
+    create_relation_order = []
+    create_relation_order.append(order)
+
+    # 执行命令存入neo4j数据库
+    for order in create_entity_order:
+        graph.run(order)
+    for order in create_relation_order:
+        graph.run(order)
+
+if __name__ == "__main__":
+    scriptToNeo()
